cd41c863ad0ea2ee57cb189e584d195326b7d76e
LeeRoiii <Leeroiaraya0@gmail.com>
2025-07-02 04:08:41 +0800

    refactor: remove unused command type definitions

--- src/types/commands.d.ts
+++
@@ -1,9 +0,0 @@
-  import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js';
-
-  declare module '../commands' {
-    export interface Command {
-      data: SlashCommandBuilder;
-      bypassRestriction?: boolean;
-      execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
-    }
-  }
